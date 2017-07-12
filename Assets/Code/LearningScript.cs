f9e27448e72ad75193a901b1aa16662b5430b00d
Chritian Domingo <christian_domingo@hotmail.com>
2017-07-12 22:40:20 +0100

    First Commit

---
+++ Assets/Code/LearningScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LearningScript : MonoBehaviour {
+	int num1 = 2;
+	int num2 = 3;
+	int num3 = 7;
+	
+	// Use this for initialization
+	void Start () {
+
+		List<string> myFavoritePonies = new List<string> ();
+		myFavoritePonies.Add ("Adam");
+		myFavoritePonies.Add ("Becca");
+		myFavoritePonies.Add ("Cristy");
+		myFavoritePonies.Add ("Denise");
+
+		Debug.Log ("This List has " + myFavoritePonies.Count + " ponies.");
+
+		Debug.Log ("The Pony's name at index 1 is " + myFavoritePonies [1]);
+		Debug.Log ("The Pony's name at index 2 is " + myFavoritePonies [2]);
+		Debug.Log ("The Pony's name at index 3 is " + myFavoritePonies [3]);
+
+		bool theBearMadeBigPottyInTheWoods = false;
+
+		if (theBearMadeBigPottyInTheWoods) 
+		{
+			Debug.Log("This is true, and it's stinky, too.");
+		} else {
+			Debug.Log("Of course NOT, it's a polar bear.");
+		}
+
+
+		int ans = AddTwoNumber (num1, num2) + AddTwoNumber (num1, num3);
+		displayResult (ans);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	int AddTwoNumber(int firstNumber, int secondNumber)
+	{
+		return firstNumber + secondNumber;
+	}
+
+	void displayResult(int total)
+	{
+		Debug.Log ("The grand total is : " + total);
+	}
+
+
+
+}
---
+++ SecondGame/New Unity Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+    private void Start()
+    {
+        score = 0;
+        UpdateScore();
+        StartCoroutine (SpawnWaves());
+
+        //text properties
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+    }
+
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while(true)
+        {
+
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-6, 6), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+
+            yield return new WaitForSeconds(waveWait);
+            //can add message to screen that says next level
+
+            //game over text exit loop
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void AddScore (int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over";
+        gameOver = true;
+
+    }
+}
